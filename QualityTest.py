81f8a8ed72be446be64b125ad326f61294ef7483
Alessio Casolaro <alessiocasolaro@hotmail.it>
2022-02-27 16:48:26 +0100

    Comments

--- QualityTest.py
+++ QualityTest.py
@@ -2,25 +2,49 @@ from math import log10, sqrt
 import cv2
 import numpy as np
 
+# Import per SSIM
+import skimage
 
-def PSNR(original, compressed): #Peak signal-to-noise ratio
+
+def PSNR(original, compressed):  # Peak signal-to-noise ratio
     mse = np.mean((original - compressed) ** 2)
     if (mse == 0):  # MSE a 0 indica che non c'è rumore
-        return 100 # Quindi la qualità è 100
+        return 100  # Quindi la qualità è 100
     max_pixel = 255.0
     psnr = 20 * log10(max_pixel / sqrt(mse))
     return psnr
 
-def MSE(original, compressed): #Mean Squared Error
+
+def MSE(original, compressed):  # Mean Squared Error
     mse = np.square(np.subtract(original, compressed)).mean()
     return mse
 
-def main():
-    original = cv2.imread("original_image.png") # Immagine non compressa
-    compressed = cv2.imread("compressed_image.png", 1) # Immagine compressa
-    value = PSNR(original, compressed)
-    print(f"PSNR: {value} dB")
 
+def SSIM(original, compressed): # Structural similarity index
+    # Conversione dell'immagine in scala di grigi
+    originalGray = cv2.cvtColor(original, cv2.COLOR_BGR2GRAY)
+    compressedGray = cv2.cvtColor(compressed, cv2.COLOR_BGR2GRAY)
+
+    ssim = skimage.metrics.structural_similarity(originalGray, compressedGray,
+                                                  gaussian_weights=True, sigma=1.5,
+                                                  use_sample_covariance=False, data_range=255)
+    if ssim == 1.0:
+        print("I frame forniti sono uguali")
+    return ssim
+
+
+def main():
+    original = cv2.imread("Resources/FramesControlLocale/frame72.jpg")  # Immagine non compressa
+    compressed = cv2.imread("Resources/5MBControlStadia/frame7.jpg", 1)  # Immagine compressa
+
+    valuePSNR = PSNR(original, compressed)
+    valueMSE = MSE(original, compressed)
+    valueSSIM = SSIM(original, compressed)
+
+    print("PSNR: ", valuePSNR)
+    print("MSE: ", valueMSE)
+    print("SSIM: ", valueSSIM)
+                                                                TEMPO CHE INTERCORRE TRA OGNI FRAME 16.6ms
 
 if __name__ == "__main__":
     main()
--- VideoAnalyzer.py
+++ VideoAnalyzer.py
@@ -1,23 +1,22 @@
-import cv2 #Libreria lavorare con i video
+import cv2  # Libreria lavorare con i video
 import math
 #############################################################
-#Script per il recupero dei frame dalle registrazioni video #
+# Script per il recupero dei frame dalle registrazioni video #
 #############################################################
-path = ".." #path dove salvare i frame
-videoName = "nomevideo.mp4" #Dove recuperare il video
+videoName = "E:/Università/Reti/Progetto Reti Cloud/Nostro/Control/5 MB/Stadia/Control-Stadia-QualityTest-5MBLimiter.mkv"  # Dove recuperare il video
 
-cap = cv2.VideoCapture(videoName) #Utilizzato per l'apertura del video acquisito
-frameRate = cap.get(5) #Utilizzato per acquisire il framerate del video cv2.CAP_PROP_FPS
+cap = cv2.VideoCapture(videoName)  # Utilizzato per l'apertura del video acquisito
+frameRate = cap.get(5)  # Utilizzato per acquisire il framerate del video cv2.CAP_PROP_FPS
 
 count = 0
-while cap.isOpened(): #Finchè il video non è finito
-    frameNum = cap.get(1)#Numero frame CAP_PROP_POS_FRAMES
+while cap.isOpened():  # Finché il video non è finito
+    frameNum = cap.get(1)  # Numero frame CAP_PROP_POS_FRAMES
     ret, frame = cap.read()
-    if(ret != True):
+    if (ret != True):
         break
-    if(frameNum % math.floor(frameRate) == 0):
-        videoPath = "frame%d.jpg" %count
-        count +=1
-        cv2.imwrite(videoName, frame)
+    if frameNum % math.floor(frameRate) == 0:
+        frameName = "Resources/5MBControlStadia/frame%d.jpg" % count
+        count += 1
+        cv2.imwrite(frameName, frame)
 cap.release()
 print("Frame acquisiti")
